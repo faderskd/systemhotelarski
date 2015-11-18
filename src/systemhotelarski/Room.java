e375703f3fb99ff2ca116fb0eb9cc49c24d8eacf
daniel.faderski <daniel.faderski@jmr.pl>
2015-11-18 16:51:44 +0100

    testy

--- src/systemhotelarski/Room.java
+++ src/systemhotelarski/Room.java
@@ -1,5 +1,6 @@
 package systemhotelarski;
 
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -34,4 +35,31 @@ public class Room {
     public void setNumber(String number) {
         this.number = number;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.number);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Room other = (Room) obj;
+        if (!this.number.equals(other.number)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
